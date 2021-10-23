eccb887a6e32b5e163063c0e08d7899315a91a26
Michaël <mika89700@gmail.com>
2021-10-23 13:52:12 +0200

    Update function.py

--- function.py
+++ function.py
@@ -1,8 +1,7 @@
 import math
 from qiskit import QuantumCircuit, execute
-from os import path
-import pandas as pd
-import datetime
+from os
+import requests
 
 
 def quantum_fight(psi, backend_sim):
@@ -124,59 +123,26 @@ def control_input(options, input_ctl):
 
 
 def stats(winner):
-    file_csv = 'stats/qpokemon_results.csv'
-    date_x = datetime.datetime.now()
-    date_day = str(date_x.month) + '-' + str(date_x.year)
+    TOKEN = os.environ.get("GITHUB_TOKEN")
+    if TOKEN != None:
+        headers = {
+          "Accept": "application/vnd.github.v3+json",
+          "Authorization": f"token {TOKEN}",
+        }
 
-    robot_csv = []
-    human_csv = []
-    date_csv = []
+        data = {
+          "event_type": "games-stats",
+          "client_payload": {
+            "game": "qpokemon",
+            "winner": winner
+          }
+        }
 
-    ##################################################################################
-    # CSV exist
-    if path.exists(file_csv):
+        requests.post(
+          f"https://api.github.com/repos/mickahell/robots-data/dispatches",
+          data=data,
+          headers=headers
+        )
 
-        csv_file = pd.read_csv(file_csv, header=None)
-
-        for i in range(len(csv_file[0])):
-            robot_csv.append(csv_file[0][i])
-            human_csv.append(csv_file[1][i])
-            date_csv.append(csv_file[2][i])
-
-        test = 0
-        for i in range(len(date_csv)):
-            if date_day == date_csv[i]:
-                if winner == "robot":
-                    robot_csv[i] += 1
-                else:
-                    human_csv[i] += 1
-                test = 1
-
-        if test == 0:
-            if winner == "robot":
-                robot_csv.append(1)
-                human_csv.append(0)
-            else:
-                robot_csv.append(0)
-                human_csv.append(1)
-            date_csv.append(date_day)
-
-        csv_file = {'robot': robot_csv, 'human': human_csv, 'date': date_csv}
-        df = pd.DataFrame(csv_file)
-        df.to_csv(file_csv, index=False, header=None)
-
-    ##################################################################################
-    # CSV doesn't exist
-    if not path.exists(file_csv):
-
-        if winner == "robot":
-            robot_csv.append(1)
-            human_csv.append(0)
-        else:
-            robot_csv.append(0)
-            human_csv.append(1)
-        date_csv.append(date_day)
-
-        csv_file = {'robot': robot_csv, 'human': human_csv, 'date': date_csv}
-        df = pd.DataFrame(csv_file)
-        df.to_csv(file_csv, index=False, header=None)
+    else:
+        print("Your token is empty ! The stats aren't updated.")
