b7f9c41ea32ff69c89a8696ccad128119245d6a3
Michaël <mika89700@gmail.com>
2021-11-08 17:39:03 +0100

    Update player.py

--- class_custom/player.py
+++ class_custom/player.py
@@ -2,13 +2,15 @@ class Player:
     """Class of player :
     - name
     - status (J1 // J2)
+    - being
     - action
     - pokemon
     - team"""
 
-    def __init__(self, name, status):
+    def __init__(self, name, status, being):
         self.name = name
         self.status = status
+        self.being = being
         self.action = 0
         self.team = []
 
